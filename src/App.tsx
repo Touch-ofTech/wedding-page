e787b962f25bdbbb7ad0bc8607d033eb91f6883d
Carlos Damian <dapecarlos@gmail.com>
2023-11-19 19:43:13 -0600

    fix(app): fixed some text (#22)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import useAudio from 'beautiful-react-hooks/useAudio';
+import arco from './assets/arco4.mp3';
 import { Header } from './sections/header/Header';
 import { Middle } from './sections/middle/Middle';
 import { Toaster } from 'react-hot-toast';
 import { AiFillSound, AiOutlinePause } from 'react-icons/ai';
-import useAudio from 'beautiful-react-hooks/useAudio';
-import arco from './assets/arco4.mp3';
 import { Dress } from './sections/dress/Dress';
 import { Confirmation } from './sections/confirmation/Confirmation';
 import { Footer } from './sections/footer/Footer';
 import './App.scss';
 
 function App() {
-  const [state, controls] = useAudio(arco, { autoPlay: true });
+  const [state, controls] = useAudio(arco, { autoPlay: true, loop: true });
 
   const handleSound = (audElemnt: any) => {
     if (audElemnt.isPlaying === false) {
